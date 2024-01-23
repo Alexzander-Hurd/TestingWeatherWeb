a2fc03ea1073c6fbcf62ee95e49fe0c7f37e875e
Alex <ajghurd@gmail.com>
2024-01-23 09:27:42 +0000

    added form for lat and long entry

--- Models/WeatherViewModel.cs
+++ Models/WeatherViewModel.cs
@@ -2,6 +2,8 @@ namespace WeatherWeb.Models.DataModels;
 
 public class WeatherViewModel
 {
-    public List<Period> periods {get; set;}
+    public List<Period> periods { get; set; }
     public string location { get; set; }
+    public string Lat { get; set; }
+    public string Long { get; set; }
 }
